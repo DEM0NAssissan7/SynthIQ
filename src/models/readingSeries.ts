d50e2823bcff7c5d4eb7dc7a8f157f931b4f2857
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2025-05-18 18:52:56 -0500

    add reading series

---
+++ src/models/readingSeries.ts
@@ -0,0 +1,29 @@
+/* This class is an auto-populating series that pulls glucose
+ * data from nightscout on its own
+ */
+
+import Series, { Color } from "./series";
+import NightscoutManager from "../lib/nightscoutManager";
+import { getHourDiff } from "../lib/util";
+
+class ReadingSeries extends Series {
+  timestamp: Date;
+  complete: boolean = false;
+
+  constructor(color: Color, timestamp: Date) {
+    super(color);
+    this.timestamp = timestamp;
+  }
+  async populate(from: Date, until: Date) {
+    NightscoutManager.getReadings(from, until).then((result: any) => {
+      result.forEach((r: any) => {
+        let sugar: number = r.sgv;
+        let timestamp: Date = new Date(r.date);
+        console.log(r, sugar);
+        this.point(getHourDiff(this.timestamp, timestamp), sugar);
+      });
+    });
+  }
+}
+
+export default ReadingSeries;
