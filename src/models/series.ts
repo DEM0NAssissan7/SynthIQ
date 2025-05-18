85bd80a6e71ac9b8c1ea55e27ec14f505014a79e
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2025-05-18 18:50:41 -0500

    add subscriptions and rechart data creation

--- src/models/series.ts
+++ src/models/series.ts
@@ -9,15 +9,27 @@ export enum Color {
   LightBlue = "light blue",
 }
 
+type SubscriberCallback = () => void;
+
 class Series {
   points: Point[] = [];
   color: Color;
+  callbacks: SubscriberCallback[] = [];
+
   constructor(color: Color = Color.Black) {
     this.color = color;
   }
   point(x: number, y: number): void {
+    for (let p of this.points) {
+      if (p[0] === x) {
+        p[1] = y;
+        this.notify();
+        return;
+      }
+    }
     let point: Point = [x, y];
     this.points.push(point);
+    this.notify();
   }
   at(x: number): number {
     // Find the closest point to the point at x
@@ -32,6 +44,25 @@ class Series {
       }
     });
     return y;
+  }
+  getRechartData(): object {
+    return this.points.map((p) => {
+      return {
+        x: p[0],
+        y: p[1],
+      };
+    });
+  }
+
+  // Subscriptions
+  subscribe(callback: SubscriberCallback): void {
+    this.callbacks.push(callback);
+  }
+  unsubscribe(callback: SubscriberCallback): void {
+    this.callbacks = this.callbacks.filter((l) => l !== callback);
+  }
+  notify(): void {
+    this.callbacks.forEach((a) => a());
   }
 }
 
