5438df270f0cc5fcac7092d792475e39d1e20c90
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2025-05-18 18:52:28 -0500

    add mathseries

--- src/models/mathSeries.ts
+++ src/models/mathSeries.ts
@@ -1,9 +1,27 @@
-import Series from "./series";
+import Series, { Color } from "./series";
+
+export type SeriesFunction = (t: number) => number;
 
 class MathSeries extends Series {
-  functions: Function[] = [];
+  functions: SeriesFunction[];
 
-  constructor() {
-    super();
+  constructor(color: Color, functions: SeriesFunction[]) {
+    super(color);
+    this.functions = functions;
+  }
+  addFunction(f: SeriesFunction) {
+    this.functions.push(f);
+  }
+  sumFunctions(t: number): number {
+    let retval = 0;
+    this.functions.map((f) => (retval += f(t)));
+    return retval;
+  }
+  populate(a: number, b: number, interval: number) {
+    for (let x = a; x < b; x += interval) {
+      this.point(x, this.sumFunctions(x));
+    }
   }
 }
+
+export default MathSeries;
