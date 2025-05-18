72e132b0ab8e67faa383b2f46709cf85dd387fbe
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2025-05-18 18:52:11 -0500

    testing graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import ListGroup from "./components/ListGroup";
+import Graph from "./components/Graph";
+import ReadingSeries from "./models/readingSeries";
+import { Color } from "./models/series";
 
 function App() {
+  const time = new Date("14:59 5-18-2025");
+  let s = new ReadingSeries(Color.Blue, time);
+  s.populate(new Date("14:00 5-18-2025"), new Date("20:00 5-18-2025"));
+
+  console.log(s);
+  const carbs = 10;
+  const protein = 40;
+
   return (
     <div>
-      <ListGroup></ListGroup>
+      <Graph series={[s]}></Graph>
     </div>
   );
 }
