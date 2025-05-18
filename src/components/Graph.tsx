ca97ec3134d3075ed600402f3b7f878d8b4ee665
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2025-05-18 18:51:56 -0500

    add recharts

--- src/components/Graph.tsx
+++ src/components/Graph.tsx
@@ -1,15 +1,51 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Series from "../models/series";
 
-class Graph extends React.Component {
-  series: Series[] = [];
-  max: number = 10;
-  min: number = -10;
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from "recharts";
 
-  addSeries() {}
-  render() {
-    return <h2>Hi, I am a Car!</h2>;
-  }
+interface GraphProps {
+  series: Series[];
+  min?: string;
 }
 
-export default Graph;
+export default function Graph({ series, min }: GraphProps) {
+  const [, setVersion] = useState(0);
+
+  useEffect(() => {
+    // for each series, subscribe once on mount...
+    const callbacks = series.map(() => () => setVersion((v) => v + 1));
+
+    series.forEach((s, i) => {
+      s.subscribe(callbacks[i]);
+    });
+
+    return () => {
+      series.forEach((s, i) => {
+        s.unsubscribe(callbacks[i]);
+      });
+    };
+  });
+
+  return (
+    <LineChart width={600} height={300}>
+      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+
+      {
+        // Create a line set for each piece of data
+        series.map((s) => {
+          return (
+            <Line
+              type="monotone"
+              data={s.getRechartData()}
+              dataKey="y"
+              stroke={s.color}
+            ></Line>
+          );
+        })
+      }
+
+      <XAxis type="number" dataKey="x" domain={["auto", "auto"]} />
+      <YAxis type="number" domain={[min || "auto", "auto"]} />
+    </LineChart>
+  );
+}
