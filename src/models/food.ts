2f154499429e910a4f839fd6113e28f167c8ba8e
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2025-05-18 18:50:16 -0500

    add deltaBG to food

--- src/models/food.ts
+++ src/models/food.ts
@@ -1,24 +1,71 @@
 import Unit from "./unit";
+import { profile } from "../lib/metabolism";
+import MetaFunctions from "./metaFunctions";
+import { metaKernel } from "./metaFunctions";
 
-class Food {
+const defaultGI = 20;
+
+export default class Food {
   name: string;
   carbsRate: number;
   proteinRate: number;
   fatRate: number = 0;
   unit: Unit.Food;
+  GI: number; // Glycemic Index (carbs only)
 
   amount: number = 0;
   constructor(
     name: string,
     carbsRate: number,
     proteinRate: number,
-    unit: Unit.Food = Unit.Food.Grams
+    unit: Unit.Food = Unit.Food.Grams,
+    GI: number = defaultGI
   ) {
     this.name = name;
     this.carbsRate = carbsRate;
     this.proteinRate = proteinRate;
     this.unit = unit;
+    this.GI = GI;
   }
+  deltaBG(t: number): number {
+    //
+    return (
+      metaKernel(
+        t,
+        this.getCarbs() * profile.get("ecarbs"),
+        profile.get("ninsulin"),
+        profile.get("pcarbs") * (defaultGI / this.GI),
+        MetaFunctions.G
+      ) +
+      metaKernel(
+        t,
+        this.getProtein() * profile.get("eprotein"),
+        profile.get("nprotein"),
+        [
+          profile.get("rprotein"), // Rise
+          profile.get("pprotein"), // Plateu
+          profile.get("fprotein"), // Fall
+        ],
+        MetaFunctions.P
+      )
+    );
+  }
+  getCarbs() {
+    return (this.carbsRate / this.unit) * this.amount;
+  }
+  getProtein() {
+    return (this.proteinRate / this.unit) * this.amount;
+  }
+
+  // static stringify(f: Food): string {
+  //   // TODO
+  //   return "";
+  // }
+  // static parse(string: string): Food {
+  //   // TODO
+  //   return;
+  //   // return new Food();
+  // }
 }
 
-export default Food;
+export const foods: Food[] = [];
