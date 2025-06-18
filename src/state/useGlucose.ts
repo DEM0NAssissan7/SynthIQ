13faa4c04778dda0af2cb031be3ee1389645cd16
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2025-06-18 18:07:59 +0300

    add state to use glucose

---
+++ src/state/useGlucose.ts
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { getMinuteDiff, getTimestampFromOffset } from "../lib/timing";
+import type MetaEvent from "../models/event";
+import type Glucose from "../models/glucose";
+
+export default function useGlucose(glucose: Glucose, event: MetaEvent) {
+  const [, setVersion] = useState(0);
+  const rerender = () => setVersion((v) => v + 1);
+  const notify = () => {
+    event.notify();
+    rerender();
+  };
+  let offset = getMinuteDiff(glucose.timestamp, event.timestamp);
+  return {
+    caps: glucose.caps,
+    timestamp: glucose.timestamp,
+    offset: offset,
+    setCaps: (caps: number) => {
+      glucose.caps = caps;
+      notify();
+    },
+    setTimestamp: (timestamp: Date) => {
+      glucose.timestamp = timestamp;
+      notify();
+    },
+    setTimestampFromOffset: (minutes: number) => {
+      offset = minutes; // This is kinda cheating, but it helps with the text box
+      glucose.timestamp = getTimestampFromOffset(event.timestamp, minutes / 60);
+      notify();
+    },
+  };
+}
