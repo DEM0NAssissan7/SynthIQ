185393a48d519e8ec7a4d4e151e9d616cda9478e
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2025-07-16 02:43:02 +0300

    add preferencesStore

---
+++ src/storage/preferencesStore.ts
@@ -0,0 +1,15 @@
+import StorageNode from "../lib/storageNode";
+
+const preferencesStore = new StorageNode("preferences");
+export default preferencesStore;
+
+preferencesStore.add("maxSessionLength", 8);
+preferencesStore.add("endingHours", 1);
+preferencesStore.add("lowBG", 67);
+preferencesStore.add("dangerBG", 55);
+
+preferencesStore.add("insulinStepSize", 1);
+preferencesStore.add("timeStepSize", 5);
+
+preferencesStore.add("sessionHalfLife", 3);
+preferencesStore.add("maxSessionLife", 14);
