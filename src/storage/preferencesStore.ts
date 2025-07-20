08fa3bbd791ef04ce97b4f640abe3c69cabd99bc
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2025-07-20 17:04:10 -0400

    add highBG preference

--- src/storage/preferencesStore.ts
+++ src/storage/preferencesStore.ts
@@ -8,6 +8,8 @@ preferencesStore.add("endingHours", 1);
 preferencesStore.add("lowBG", 67);
 preferencesStore.add("dangerBG", 55);
 
+preferencesStore.add("highBG", 125);
+
 preferencesStore.add("insulinStepSize", 1);
 preferencesStore.add("timeStepSize", 5);
 
