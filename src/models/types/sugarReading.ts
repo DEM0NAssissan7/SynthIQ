ef375e1692847fcda1841660ce2801570f91040b
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2025-08-12 15:36:14 -0500

    add iscalibration parameter to sugarreading to discern between reliable and less-reliable readings

--- src/models/types/sugarReading.ts
+++ src/models/types/sugarReading.ts
@@ -1,20 +1,32 @@
 export default class SugarReading {
-  constructor(public sugar: number, public timestamp: Date) {}
+  constructor(
+    public sugar: number,
+    public timestamp: Date,
+    public isCalibration: boolean = false
+  ) {}
 
   static stringify(r: SugarReading) {
     return JSON.stringify({
       sugar: r.sugar,
       timestamp: r.timestamp.toString(),
+      isCalibration: r.isCalibration,
     });
   }
   static parse(s: string) {
     const o = JSON.parse(s);
-    return new SugarReading(o.sugar, new Date(o.timestamp));
+    return new SugarReading(
+      o.sugar,
+      new Date(o.timestamp),
+      o.isCalibration || false
+    );
   }
 }
 
-export function getReadingFromNightscout(o: any): SugarReading {
-  return new SugarReading(o.sgv, new Date(o.date));
+export function getReadingFromNightscout(o: {
+  sgv: number;
+  date: string;
+}): SugarReading {
+  return new SugarReading(o.sgv, new Date(o.date), false);
 }
 export function createNightscoutReading(r: SugarReading) {
   return {
