faee7878ab7cd471b38c4df59ad455e33489d418
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2025-10-11 12:03:52 -0500

    update useNow to take seconds as the input instead of ms

--- src/state/useNow.ts
+++ src/state/useNow.ts
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 
-export function useNow(updateRate = 10000) {
+export function useNow(updateRate = 10) {
   const [now, setNow] = useState<Date>(new Date());
 
   useEffect(() => {
     const interval = setInterval(() => {
       setNow(new Date());
-    }, updateRate);
+    }, updateRate * 1000);
     return () => clearInterval(interval);
   }, [updateRate]);
   return now;
