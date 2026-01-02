1be4e76c97abb7cb1b944f42a6b5df2dfd638945
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2026-01-02 15:46:53 -0600

    add queue information and clearing in the debug page

--- src/pages/DebugPage.tsx
+++ src/pages/DebugPage.tsx
@@ -1,15 +1,49 @@
+import { Button } from "react-bootstrap";
 import Card from "../components/Card";
 import StorageBackends from "../registries/storageBackends";
+import { BackendStore } from "../storage/backendStore";
 
 export default function DebugPage() {
   const storageBackend = StorageBackends.getDefault();
+  const [queue, setQueue] = BackendStore.queue.useState();
 
+  function clearQueue() {
+    if (confirm(`Are you sure you want to clear the remote request queue?`)) {
+      setQueue([]);
+    }
+  }
   return (
     <>
       <h1>Debug</h1>
       <Card>
         <h3>Storage Backend</h3>
         Storage size: {(storageBackend.size / 1024).toFixed(1)} KB
+      </Card>
+      <Card>
+        <h3>Remote Queue</h3>
+        <Button variant={"outline-danger"} onClick={clearQueue}>
+          Clear Queue
+        </Button>
+        <table>
+          <thead>
+            <tr>
+              <th>API</th>
+              <th>Payload Size</th>
+              <th>Type</th>
+              <th>UUID</th>
+            </tr>
+          </thead>
+          <tbody>
+            {queue.map((q) => (
+              <tr key={q.uuid}>
+                <td>{q.api}</td>
+                <td>{q.payload?.length ?? 0}</td>
+                <td>{q.type}</td>
+                <td>{q.uuid}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </Card>
     </>
   );
