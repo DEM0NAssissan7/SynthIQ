5ace21211d5aa973fd522e3f32b94f9f88509656
Abdurahman Elmawi <abdurahman.elmawi06@gmail.com>
2026-01-03 10:37:42 -0600

    add .suffix getter

--- src/models/types/rescueVariant.ts
+++ src/models/types/rescueVariant.ts
@@ -1,20 +1,23 @@
 import type { Deserializer, Serializer } from "./types";
 
 export class RescueVariant {
-    constructor(
-        public name: string,
-        public duration: number,
-        public effect: number
-    ) { }
+  constructor(
+    public name: string,
+    public duration: number,
+    public effect: number
+  ) {}
+  get suffix() {
+    return this.name.toLowerCase()[0];
+  }
 
-    static serialize: Serializer<RescueVariant> = (i: RescueVariant) => {
-        return {
-            name: i.name,
-            duration: i.duration,
-            effect: i.effect,
-        };
+  static serialize: Serializer<RescueVariant> = (i: RescueVariant) => {
+    return {
+      name: i.name,
+      duration: i.duration,
+      effect: i.effect,
     };
-    static deserialize: Deserializer<RescueVariant> = (o) => {
-        return new RescueVariant(o.name, o.duration, o.effect);
-    };
+  };
+  static deserialize: Deserializer<RescueVariant> = (o) => {
+    return new RescueVariant(o.name, o.duration, o.effect);
+  };
 }
